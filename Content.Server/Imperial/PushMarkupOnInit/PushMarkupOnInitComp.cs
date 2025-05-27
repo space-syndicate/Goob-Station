337ad46c42f3d20296d84f1d70125e34fe4c647b
LightLava <160842994+LightLava@users.noreply.github.com>
2025-05-27 14:29:00 +0700

    Add files via upload

---
+++ Content.Server/Imperial/PushMarkupOnInit/PushMarkupOnInitComp.cs
@@ -0,0 +1,8 @@
+namespace Content.Server.Imperial.PushMarkupOnInit;
+
+[RegisterComponent]
+public sealed partial class PushMarkupOnInitComponent : Component
+{
+    [DataField]
+    public string Markup;
+}
