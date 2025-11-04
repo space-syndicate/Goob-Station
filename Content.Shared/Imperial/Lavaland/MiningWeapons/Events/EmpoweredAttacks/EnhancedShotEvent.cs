24ef5a6441dd8050b496e52879d2226f51283a14
Koting <jekalavrovih@gmail.com>
2025-11-04 11:36:57 +0300

    feat(MiningWeapons): events EmpoweredAttacks

---
+++ Content.Shared/Imperial/Lavaland/MiningWeapons/Events/EmpoweredAttacks/EnhancedShotEvent.cs
@@ -0,0 +1,10 @@
+using Content.Shared.Actions;
+
+namespace Content.Shared.Imperial.Lavaland.MiningWeapons.Events.EmpoweredAttacks;
+
+public sealed partial class EnhancedShotEvent : WorldTargetActionEvent
+{
+    /// todo: sound
+    /// todo: damage change
+    /// todo: doafter?
+}
---
+++ Content.Shared/Imperial/Lavaland/MiningWeapons/Events/EmpoweredAttacks/PiercingLungeEvent.cs
@@ -0,0 +1,10 @@
+using Content.Shared.Actions;
+
+namespace Content.Shared.Imperial.Lavaland.MiningWeapons.Events.EmpoweredAttacks;
+
+public sealed partial class PiercingLungeEvent : WorldTargetActionEvent
+{
+    /// todo: sound
+    /// todo: damage
+    /// todo: doafter?
+}
