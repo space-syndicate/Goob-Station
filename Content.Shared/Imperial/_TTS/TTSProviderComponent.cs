0fbb1fdc98771b297cc703bc38a1aa271e80b02b
ArturUr <arturik.bj@yandex.ru>
2025-11-29 15:37:17 +0300

    feat(TTSProviderComponent): Partly moved from private in publik

---
+++ Content.Shared/Imperial/_TTS/TTSProviderComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Imperial.TTS;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class TTSProviderComponent : Component
+{
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField("voice")]
+    public string Voice = default!;
+}
