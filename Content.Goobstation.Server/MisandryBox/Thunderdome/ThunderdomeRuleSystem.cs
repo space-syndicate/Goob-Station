faa7b88d6058f5d6957692fc1b38b0291536db0b
Dmitry <57028746+DIMMoon1@users.noreply.github.com>
2026-03-15 17:44:18 +0700

    Revert "thunderdome update (#646)"
    This reverts commit fb5dc0577d279099532413eea29cbf8b5ed81499.

--- Content.Goobstation.Server/MisandryBox/Thunderdome/ThunderdomeRuleSystem.cs
+++ Content.Goobstation.Server/MisandryBox/Thunderdome/ThunderdomeRuleSystem.cs
@@ -185,13 +185,6 @@ public sealed class ThunderdomeRuleSystem : EntitySystem
         if (!_cfg.GetCVar(ThunderdomeCVars.ThunderdomeEnabled))
             return;
 
-        // CorvaxGoob-Thunderdome-start
-        var duration = _ticker.RoundDuration();
-        if (_cfg.GetCVar(ThunderdomeCVars.ActivationDelayEnabled) &&
-            (_cfg.GetCVar(ThunderdomeCVars.ActivationDelay) > (int) duration.TotalMinutes))
-            return;
-        // CorvaxGoob-Thunderdome-end
-
         EnsureRule();
 
         if (_ruleEntity == null
