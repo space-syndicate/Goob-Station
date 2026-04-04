fc956c9ef0b1696865a4f26791810be597ff113b
WardexOfficial <132839034+WardexOfficial@users.noreply.github.com>
2026-04-04 04:06:32 +0300

    Забыл

--- Content.Server/Imperial/Thief/ThiefRoleSystem.Restrictions.cs
+++ Content.Server/Imperial/Thief/ThiefRoleSystem.Restrictions.cs
@@ -1,19 +1,11 @@
-using Content.Shared.Roles;
-using Content.Shared.Roles.Components;
 using Content.Server.Ame.Components;
 using Content.Server.ParticleAccelerator.Components;
-using Content.Shared.Popups;
 using Content.Shared.Interaction.Events;
-using Content.Shared.Mind;
-using Content.Shared.Ghost;
 using Content.Shared.Imperial.Thief;
 
 namespace Content.Server.Imperial.Thief;
 public sealed class ThiefRoleSystem : EntitySystem
 {
-    [Dependency] private readonly SharedRoleSystem _roles = default!;
-    [Dependency] private readonly SharedPopupSystem _popup = default!;
-    [Dependency] private readonly SharedMindSystem _mind = default!;
     [Dependency] private readonly SharedThiefRoleSystem _sharedThiefRestrictions = default!;
 
     public override void Initialize()
