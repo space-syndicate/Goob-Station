e829119b2ad1cea0d19a217b347e4945fc50bff8
Wardex <grichanijbogdan@gmail.com>
2026-04-04 23:25:27 +0300

    -хардкод, доработка MindPacifiedComponent

---
+++ Content.Shared/Imperial/MindPacified/Components/MindPacifiedComponent.cs
@@ -0,0 +1,14 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Imperial.MindPacified.Components;
+
+/// <summary>
+/// Компонент, использующийся для пометки того разума сущности,
+/// которая должна получить пацифизм после перемещения разума.
+/// </summary>
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class MindPacifiedComponent : Component
+{
+
+}
--- Content.Shared/Imperial/MindPacified/MindPacifiedSystem.cs
+++ Content.Shared/Imperial/MindPacified/MindPacifiedSystem.cs
@@ -1,23 +1,26 @@
-using Content.Shared.Mind.Components;
 using Content.Shared.CombatMode.Pacification;
 using Content.Shared.Imperial.AutoPacified.Components;
+using Content.Shared.Mind;
+using Content.Shared.Mind.Components;
 
 namespace Content.Shared.Imperial.AutoPacified;
 public sealed class AutoPacifiedSystem : EntitySystem
 {
-
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<MindAddedMessage>(OnMindAdded);
+        SubscribeLocalEvent<MindGotAddedEvent>(OnMindAdded);
     }
 
-    private void OnMindAdded(MindAddedMessage args)
+    private void OnMindAdded(MindGotAddedEvent args)
     {
+        if (args.TransferEntity == null)
+            return;
+
         if (HasComp<AutoPacifiedComponent>(args.Mind))
         {
-            EnsureComp<PacifiedComponent>(args.Container);
+            EnsureComp<PacifiedComponent>(args.TransferEntity.Value);
         }
     }
 }
