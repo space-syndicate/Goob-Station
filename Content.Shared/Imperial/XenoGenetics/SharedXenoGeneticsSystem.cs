181bc7e8fb8ae003bd74a946eafd0f09be6451a2
LightLava <pasha.notch@gmail.com>
2026-04-05 17:17:12 +0700

    пожалуйста хватит

--- Content.Shared/Imperial/XenoGenetics/SharedXenoGeneticsSystem.cs
+++ Content.Shared/Imperial/XenoGenetics/SharedXenoGeneticsSystem.cs
@@ -55,7 +55,7 @@ public abstract class SharedXenoGeneticsSystem : EntitySystem
 
         if (component.RandomizeGeneQuality)
         {
-            float multiplier;
+            var multiplier;
             var quality = _rand.Next(0, 10);
             switch (quality)
             {
