d5c7a113da71b6ed7b2ff5a701497787e177b3c0
Swino4ka <83330628+Swino4ka@users.noreply.github.com>
2026-06-27 22:50:27 +0200

    Хочу чтоб таракомоли могли расти очин балшиме

--- Content.Goobstation.Shared/EatToGrow/EatToGrowComponent.cs
+++ Content.Goobstation.Shared/EatToGrow/EatToGrowComponent.cs
@@ -6,16 +6,16 @@ namespace Content.Goobstation.Shared.EatToGrow;
 public sealed partial class EatToGrowComponent : Component
 {
     [DataField]
-    public float Growth = 0.1f; // percentage growth
+    public float Growth = 0.001f; // percentage growth
 
     [DataField]
-    public float MaxGrowth = 5.0f; // max allowed scale multiplier
+    public float MaxGrowth = 500.0f; // max allowed scale multiplier
 
     [DataField]
     public float CurrentScale = 1.0f; // current scale
 
     [DataField]
-    public bool ShrinkOnDeath = true; // Revert to original size on death?
+    public bool ShrinkOnDeath = false; // Revert to original size on death?
 
     [DataField]
     public int TimesGrown = 0; // how many times have they grown?
