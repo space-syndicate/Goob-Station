3effbf5d85cb0a3210a969d19c9e32125f00f2b0
Swino4ka <83330628+Swino4ka@users.noreply.github.com>
2026-06-28 20:31:40 +0200

    Сделал по нормальному 🍉

--- Content.Goobstation.Shared/EatToGrow/EatToGrowComponent.cs
+++ Content.Goobstation.Shared/EatToGrow/EatToGrowComponent.cs
@@ -9,13 +9,13 @@ public sealed partial class EatToGrowComponent : Component
     public float Growth = 0.1f; // percentage growth
 
     [DataField]
-    public float MaxGrowth = 500.0f; // max allowed scale multiplier|CorvaxGoob: 5--->500
+    public float MaxGrowth = 5.0f; // max allowed scale multiplier
 
     [DataField]
     public float CurrentScale = 1.0f; // current scale
 
     [DataField]
-    public bool ShrinkOnDeath = false; // Revert to original size on death?|CorvaxGoob: true--->false
+    public bool ShrinkOnDeath = true; // Revert to original size on death?
 
     [DataField]
     public int TimesGrown = 0; // how many times have they grown?
