f16d73333fdc34fcebb816b15216dd009a3a7e27
username <32684670+Snorkcom@users.noreply.github.com>
2026-07-29 20:20:44 +0600

    fix License and margin

--- Content.Server/_CorvaxGoob/Radio/HeadsetSystem.RadioChannelSounds.cs
+++ Content.Server/_CorvaxGoob/Radio/HeadsetSystem.RadioChannelSounds.cs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 
 using Content.Server.Radio;
 using Content.Shared._CorvaxGoob.Radio;
--- Content.Shared/_CorvaxGoob/Radio/SharedHeadsetSystem.RadioChannelSounds.cs
+++ Content.Shared/_CorvaxGoob/Radio/SharedHeadsetSystem.RadioChannelSounds.cs
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: MIT
 
 using Content.Shared.Radio.Components;
 using Robust.Shared.Prototypes;
